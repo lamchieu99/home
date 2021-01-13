edb2fc8746d2edce0987b26f0f945cd958ccce4f
LamChieu <52293205+LamChieu@users.noreply.github.com>
2021-01-13 14:57:55 +0700

    something

---
+++ pages/Home/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styles from "./home.module.css"
+
+
+export default function home() {
+    return (
+        <div className={styles.categories}>
+            <div className={styles.category}>
+                <img src="https://danviet.mediacdn.vn/upload/3-2019/images/2019-09-09/5-ly-do-vi-sao-khong-nen-mua-xe-hoi-bang-gia-xe-o-to-moi-nhat-hom-nay-thang-1568002119-width800height450.jpg" alt="oto" />
+            </div>
+            <div className={styles.category}>
+                <img src="https://danviet.mediacdn.vn/upload/3-2019/images/2019-09-09/5-ly-do-vi-sao-khong-nen-mua-xe-hoi-bang-gia-xe-o-to-moi-nhat-hom-nay-thang-1568002119-width800height450.jpg" alt="oto" />
+            </div>
+            <div className={styles.category}>
+                <img src="https://danviet.mediacdn.vn/upload/3-2019/images/2019-09-09/5-ly-do-vi-sao-khong-nen-mua-xe-hoi-bang-gia-xe-o-to-moi-nhat-hom-nay-thang-1568002119-width800height450.jpg" alt="oto" />
+            </div>
+            <div className={styles.category}>
+                <img src="https://danviet.mediacdn.vn/upload/3-2019/images/2019-09-09/5-ly-do-vi-sao-khong-nen-mua-xe-hoi-bang-gia-xe-o-to-moi-nhat-hom-nay-thang-1568002119-width800height450.jpg" alt="oto" />
+            </div>
+        </div>
+    )
+}
